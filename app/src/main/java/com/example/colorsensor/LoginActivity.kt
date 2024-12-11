1102773463b0907e87291e40135ba5d165df6fb5
david_may <dmei0676@gmail.com>
2024-12-10 16:28:06 -0800

    Insert a home screen
    These are the files I know made changes.

--- app/src/main/java/com/example/colorsensor/LoginActivity.kt
+++ app/src/main/java/com/example/colorsensor/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.colorsensor
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login) // Ensure the correct layout file is referenced here
+
+        // Find the button by its ID
+        val btnGoToHome = findViewById<Button>(R.id.guest_button)
+
+        // Set a click listener for the button
+        btnGoToHome.setOnClickListener {
+            // Navigate to HomeActivity
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+//            finish() // Finish SplashActivity to prevent going back
+        }
     }
 }
