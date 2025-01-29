c220f4464f6ce593475a384220b5f32df6287985
Jason <JasonMarquez56@protonmail.com>
2025-01-29 00:03:43 -0800

    Updated home_Screen.xml

--- app/src/main/java/com/example/colorsensor/HomeActivity.kt
+++ app/src/main/java/com/example/colorsensor/HomeActivity.kt
@@ -2,18 +2,18 @@ package com.example.colorsensor
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract.Contacts.Photo
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_screen) // Ensure the correct layout file is referenced here
+        // setContentView(R.layout.home_screen) // Ensure the correct layout file is referenced here
+        setContentView(R.layout.home_screen)
 
         // Find the button by its ID
-        val profileButton = findViewById<Button>(R.id.button4) // profile
-        val photobutton = findViewById<Button>(R.id.button6) // find color
+        val profileButton = findViewById<Button>(R.id.profileButton) // profile
+        val photobutton = findViewById<Button>(R.id.photoButton) // find color
 
         // Set a click listener for the Profile button
         photobutton.setOnClickListener {
