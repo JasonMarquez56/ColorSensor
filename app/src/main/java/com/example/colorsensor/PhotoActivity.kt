596a34617f16e60e3335bfcadb8d28647abb6621
Jason <JasonMarquez56@protonmail.com>
2025-01-29 23:27:11 -0800

    Updated Home Screen, added icons, and search bar (unresponsive)

--- app/src/main/java/com/example/colorsensor/PhotoActivity.kt
+++ app/src/main/java/com/example/colorsensor/PhotoActivity.kt
@@ -1,19 +1,15 @@
 package com.example.colorsensor
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
 
 class PhotoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.find_color) // Ensure the correct layout file is referenced here
 
-        val backButton = findViewById<ImageButton>(R.id.backButton2)
+        val backButton = findViewById<ImageButton>(R.id.backButton)
 
         // Handle Back button click
         backButton.setOnClickListener {
