2ef29e6dac7185fcc5f1d086a5bc3cca326d0a7c
Jeff Kim <Jeff.kim01@student.csulb.edu>
2025-02-02 00:03:11 -0800

    Selecting from image added

--- app/src/main/java/com/example/colorsensor/PhotoActivity.kt
+++ app/src/main/java/com/example/colorsensor/PhotoActivity.kt
@@ -3,11 +3,15 @@ package com.example.colorsensor
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
+import android.widget.Toast
+import androidx.activity.result.*
+import androidx.activity.result.contract.*
+import androidx.activity.result.ActivityResultCaller
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class PhotoActivity : AppCompatActivity() {
@@ -17,18 +21,30 @@ class PhotoActivity : AppCompatActivity() {
 
         // val cameraButton = findViewById<Button>(R.id.cameraButton) // find color
         val takePhoto = findViewById<Button>(R.id.cameraButton)
+        val uploadButton = findViewById<Button>(R.id.uploadButton)
+        val imageSelector = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri")
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+        uploadButton.setOnClickListener{
+            imageSelector.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
 
-        activityResultLauncher = registerForActivityResult<Intent, ActivityResult>(
-            StartActivityForResult(),
-            object : ActivityResultCallback<ActivityResult?> {
-                override fun onActivityResult(result: ActivityResult) {
-                    if (result.resultCode == RESULT_OK && result.data != null) {
-                        val bundle = result.data!!.extras
-                        val bitmap = bundle!!["data"] as Bitmap?
-                        imageProfile.setImageBitmap(bitmap)
-                    }
-                }
-            })
+        imageSelector.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+//        activityResultLauncher = registerForActivityResult<Intent, ActivityResult>(
+//            StartActivityForResult(),
+//            object : ActivityResultCallback<ActivityResult?> {
+//                override fun onActivityResult(result: ActivityResult) {
+//                    if (result.resultCode == RESULT_OK && result.data != null) {
+//                        val bundle = result.data!!.extras
+//                        val bitmap = bundle!!["data"] as Bitmap?
+//                        imageProfile.setImageBitmap(bitmap)
+//                    }
+//                }
+//            })
 
         takePhoto.setOnClickListener(View.OnClickListener {
             val intent = Intent((MediaStore.ACTION_IMAGE_CAPTURE))
