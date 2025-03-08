31c45daf2b7389aee2e125a29aa1bedcc46b9ede
david_may <dmei0676@gmail.com>
2025-03-07 16:40:54 -0800

    Updates to popular color

--- app/src/main/java/com/example/colorsensor/PopularColor.kt
+++ app/src/main/java/com/example/colorsensor/PopularColor.kt
@@ -9,6 +9,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Magnifier
 import android.widget.Toast
@@ -21,6 +22,9 @@ import kotlin.random.Random
 class PopularColor : AppCompatActivity() {
     private var magnifier: Magnifier? = null
     private var isMagnifierActive = false // Track magnifier state
+    var favColors : MutableList<String> = mutableListOf()
+    private lateinit var firestore: FirebaseFirestore
+    var selectedFav = ""
 
     // Create a magnifier tutorial
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -38,41 +42,41 @@ class PopularColor : AppCompatActivity() {
         val targetRegion = Rect(0, 125, 1228, 809) // Adjust these values as needed
 
         // Create a magnifier tutorial
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                magnifier = Magnifier.Builder(imageView)
-                    .setInitialZoom(2.3f)
-                    .setElevation(10.0f)
-                    .setCornerRadius(20.0f)
-                    .setSize(100, 100)
-                    .build()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            magnifier = Magnifier.Builder(imageView)
+                .setInitialZoom(2.3f)
+                .setElevation(10.0f)
+                .setCornerRadius(20.0f)
+                .setSize(100, 100)
+                .build()
+        }
+
+        // Toggle magnifier on button press
+        zoomButton.setOnClickListener {
+            isMagnifierActive = !isMagnifierActive // Toggle state
+
+            if (isMagnifierActive) {
+                zoomButton.text = "Disable Magnifier"
+            } else {
+                zoomButton.text = "Enable Magnifier"
+                magnifier?.dismiss()
             }
+        }
 
-            // Toggle magnifier on button press
-            zoomButton.setOnClickListener {
-                isMagnifierActive = !isMagnifierActive // Toggle state
-
-                if (isMagnifierActive) {
-                    zoomButton.text = "Disable Magnifier"
-                } else {
-                    zoomButton.text = "Enable Magnifier"
-                    magnifier?.dismiss()
-                }
-            }
-
-            // Move the magnifier while touching the screen
-            imageView.setOnTouchListener { _, event ->
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && isMagnifierActive) {
-                    when (event.action) {
-                        MotionEvent.ACTION_MOVE, MotionEvent.ACTION_DOWN -> {
-                            magnifier?.show(event.rawX, event.y)
-                        }
-                        MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                            magnifier?.dismiss()
-                        }
+        // Move the magnifier while touching the screen
+        imageView.setOnTouchListener { _, event ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && isMagnifierActive) {
+                when (event.action) {
+                    MotionEvent.ACTION_MOVE, MotionEvent.ACTION_DOWN -> {
+                        magnifier?.show(event.rawX, event.y)
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        magnifier?.dismiss()
                     }
                 }
-                true
             }
+            true
+        }
 
         // Set random colors for 25 buttons
         for (i in 1..25) {
@@ -84,31 +88,31 @@ class PopularColor : AppCompatActivity() {
             val firestore = FirebaseFirestore.getInstance()
 
             // get all favorite color
-//            firestore.collection("users")
-//                .get()
-//                .addOnSuccessListener { documents ->
-//                    // store favorite color in a list
-//                    val allFavoriteColors = mutableListOf<String>()
-//
-//                    for (document in documents) {
-//                        val favoriteColors = document.get("favoriteColors") as? List<String>
-//                        if (favoriteColors != null) {
-//                            allFavoriteColors.addAll(favoriteColors)
-//                        }
-//                    }
-//
-//                    if (allFavoriteColors.isNotEmpty()) {
-//                        // Remove duplicates and join into a string to display
-//                        val uniqueColors = allFavoriteColors.toSet().joinToString(", ")
-//                        Toast.makeText(this, "All Favorite Colors: $uniqueColors", Toast.LENGTH_LONG).show()
-//                    } else {
-//                        Toast.makeText(this, "No favorite colors found", Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//                .addOnFailureListener { exception ->
-//                    Log.e("Firestore", "Error getting favorite colors: ", exception)
-//                    Toast.makeText(this, "Failed to fetch favorite colors", Toast.LENGTH_SHORT).show()
-//                }
+            firestore.collection("users")
+                .get()
+                .addOnSuccessListener { documents ->
+                    // store favorite color in a list
+                    val allFavoriteColors = mutableListOf<String>()
+
+                    for (document in documents) {
+                        val favoriteColors = document.get("favoriteColors") as? List<String>
+                        if (favoriteColors != null) {
+                            allFavoriteColors.addAll(favoriteColors)
+                        }
+                    }
+
+                    if (allFavoriteColors.isNotEmpty()) {
+                        // Remove duplicates and join into a string to display
+                        val uniqueColors = allFavoriteColors.toSet().joinToString(", ")
+                        Toast.makeText(this, "All Favorite Colors: $uniqueColors", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "No favorite colors found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("Firestore", "Error getting favorite colors: ", exception)
+                    Toast.makeText(this, "Failed to fetch favorite colors", Toast.LENGTH_SHORT).show()
+                }
 
             // Create a random color
             val color = Color.argb(255, newRed, newGreen, newBlue)
@@ -161,4 +165,28 @@ class PopularColor : AppCompatActivity() {
 
     // Data class for defining a rectangular area
     data class Rect(val left: Int, val top: Int, val right: Int, val bottom: Int)
+
+    private fun displayColors(favColorContainer : LinearLayout){
+        favColorContainer.removeAllViews()
+        for (color in favColors){
+            val textView = TextView(this)
+            textView.text = color
+            firestore.collection("paints")
+                .whereEqualTo("name", color)  // Query by username
+                .get()
+                .addOnSuccessListener { paints ->
+                    if (!paints.isEmpty) {
+                        for(paint in paints){
+                            val hex = paint.get("hex") as String
+                            val rgbInfo = hex.removePrefix("rgb(").removeSuffix(")").split(",")
+                            val red = rgbInfo[0].trim().toInt()
+                            val green = rgbInfo[1].trim().toInt()
+                            val blue = rgbInfo[2].trim().toInt()
+
+                            textView.setBackgroundColor(Color.rgb(red,green,blue))
+                        }
+                    }
+                }
+        }
+    }
 }
